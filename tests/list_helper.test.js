82a13f6d33388a2cb9b04d62beb12047c59f772a
satse13 <atobiasangeles@gmail.com>
2023-09-26 10:33:53 +0200

    Añadidas helper funtion with test

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -24,3 +24,97 @@ describe('total likes', () => {
 		expect(result).toBe(5)
 	})
 })
+
+describe('blog with the most likes', () => {
+	const listWithTwoBlogs = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a698734d17f8',
+			title: 'Second test',
+			author: 'Edsger W. William',
+			url: 'http://www.u.florida.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 10,
+			__v: 0
+		}
+	]
+
+	test('when list has only two blogs,gives the one with the most likes', () => {
+		const result = listHelper.favoriteBlogs(listWithTwoBlogs)
+		expect(result).toEqual(listWithTwoBlogs[1])
+	})
+})
+
+describe('author with the most blogs', () => {
+	const listWithThreeBlogs = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a698734d17f8',
+			title: 'Second blos',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.florida.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a698734d17f8',
+			title: 'Second blos',
+			author: 'John W. Dijkstra',
+			url: 'http://www.u.florida.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 10,
+			__v: 0
+		}
+	]
+  
+	test('when list has only three blogs,gives the author with the most blogs and the count of them', () => {
+		const result = listHelper.mostBlogs(listWithThreeBlogs)
+		expect(result).toEqual({maxElem : 'Edsger W. Dijkstra', maxCount : 2})
+	})
+})
+
+describe('author with the most likes', () => {
+	const listWithThreeBlogs = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a698734d17f8',
+			title: 'Second blos',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.florida.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a698734d17f8',
+			title: 'Second blos',
+			author: 'John W. Dijkstra',
+			url: 'http://www.u.florida.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 25,
+			__v: 0
+		}
+	]
+  
+	test('when list has only three blogs,gives the author with the most likes and the count of them', () => {
+		const result = listHelper.mostLikes(listWithThreeBlogs)
+		expect(result).toEqual({maxElem : 'John W. Dijkstra', maxCount : 25})
+	})
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,4 +6,46 @@ const totalLikes = (blogList) => {
   return blogList.map(blog => blog.likes).reduce((acumulator, value) => acumulator + value, 0)
 }
 
-module.exports = {dummy, totalLikes}
+const favoriteBlogs = (blogList) => {
+  const maxLikes = Math.max(...blogList.map(blog => blog.likes))
+  return blogList.find((blog) => maxLikes === blog.likes)
+}
+
+const mostBlogs = (blogList) => {
+  let maxElem = blogList[0].author
+  let maxCount = 1
+  const freqMap = {}
+  for(let i = 0; i < blogList.length;i++){
+    const elem = blogList[i].author
+    if(freqMap[elem])
+      freqMap[elem]++
+    else  
+      freqMap[elem] = 1
+    if(freqMap[elem] > maxCount){
+      maxCount = freqMap[elem]
+      maxElem = elem
+    }
+  }
+  return {maxElem, maxCount}
+
+}
+
+const mostLikes = (blogList) => {
+  let maxElem = blogList[0].author
+  let maxCount = blogList[0].likes
+  const freqMap = {}
+  for(let i = 0; i < blogList.length;i++){
+    const elem = blogList[i].author
+    if(freqMap[elem])
+      freqMap[elem] += blogList[i].likes
+    else  
+      freqMap[elem] = blogList[i].likes
+    if(freqMap[elem] > maxCount){
+      maxCount = freqMap[elem]
+      maxElem = elem
+    }
+  }
+  return {maxElem, maxCount}
+}
+
+module.exports = {dummy, totalLikes, favoriteBlogs, mostBlogs, mostLikes}
