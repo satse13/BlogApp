8425c2a89a79ff585d612745aab73764edc591dd
satse13 <atobiasangeles@gmail.com>
2023-10-07 12:49:36 +0200

    Creada base de datos de usuarios con contraseña hasheada

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const userRouter = require('./controllers/users')
 
 mongoose.set('strictQuery', false)
 
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users',userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
